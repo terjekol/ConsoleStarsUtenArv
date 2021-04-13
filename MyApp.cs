40970f02f245960ecbe5b4c1b46253898b302a18
GET Academy <terje@kolderup.net>
2021-04-13 13:51:22 +0200

    x

---
+++ MyApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStars
+{
+    class MyApp : IClickHandler
+    {
+        private MyButton _myButton;
+        int _counter;
+
+        public MyApp()
+        {
+            _myButton = new MyButton();
+            _myButton.ClickHandler = this;
+        }
+
+        public void HandleClick()
+        {
+            _counter++;
+        }
+    }
+}
---
+++ MyButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStars
+{
+    class MyButton
+    {
+        public IClickHandler ClickHandler { get; set; }
+
+        public void DoWork()
+        {
+            // if button is clicked
+            ClickHandler.HandleClick();
+        }
+    }
+}
